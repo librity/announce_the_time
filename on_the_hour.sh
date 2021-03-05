bc15a8d77715309aaf12db5e88edae6347fbaccd
Luis Geniole <luis.geniole@gmail.com>
2021-03-05 14:02:40 -0300

    Use arrays.

--- on_the_hour.sh
+++ on_the_hour.sh
@@ -3,10 +3,10 @@
 #                                                         :::      ::::::::    #
 #    on_the_hour.sh                                     :+:      :+:    :+:    #
 #                                                     +:+ +:+         +:+      #
-#    By: lpaulo-m@student.42sp.org.br <lpaulo-m>    +#+  +:+       +#+         #
+#    By: lpaulo-m <lpaulo-m@student.42sp.org.br>    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2021/02/25 19:09:29 by lpaulo-m@st       #+#    #+#              #
-#    Updated: 2021/02/25 20:53:26 by lpaulo-m@st      ###   ########.fr        #
+#    Updated: 2021/03/05 13:58:59 by lpaulo-m         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -14,59 +14,11 @@
 
 say_time_and_exit() {
     echo "It's $1 o'clock." | festival --tts
-    exit 0
 }
 
+hours=("twelve" "one" "two" "three" "four" "five" "six" "seven" "eight" "nine" "ten" "eleven" "twelve")
 current_hour=$(date +"%l")
 
-if [ "$((current_hour))" -eq 0 ]; then
-    say_time_and_exit twelve
-fi
+say_time_and_exit ${hours[current_hour]}
 
-if [ "$((current_hour))" -eq 1 ]; then
-    say_time_and_exit one
-fi
-
-if [ "$((current_hour))" -eq 2 ]; then
-    say_time_and_exit two
-fi
-
-if [ "$((current_hour))" -eq 3 ]; then
-    say_time_and_exit three
-fi
-
-if [ "$((current_hour))" -eq 4 ]; then
-    say_time_and_exit four
-fi
-
-if [ "$((current_hour))" -eq 5 ]; then
-    say_time_and_exit five
-fi
-
-if [ "$((current_hour))" -eq 6 ]; then
-    say_time_and_exit six
-fi
-
-if [ "$((current_hour))" -eq 7 ]; then
-    say_time_and_exit seven
-fi
-
-if [ "$((current_hour))" -eq 8 ]; then
-    say_time_and_exit eight
-fi
-
-if [ "$((current_hour))" -eq 9 ]; then
-    say_time_and_exit nine
-fi
-
-if [ "$((current_hour))" -eq 10 ]; then
-    say_time_and_exit ten
-fi
-
-if [ "$((current_hour))" -eq 11 ]; then
-    say_time_and_exit eleven
-fi
-
-if [ "$((current_hour))" -eq 12 ]; then
-    say_time_and_exit twelve
-fi
+exit 0
